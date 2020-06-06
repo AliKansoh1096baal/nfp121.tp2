1fc4aa29559ca4d011a3dfef1b63d65f9611a368
AliKansoh <ali.kansoh.1@isae.edu.lb>
2020-06-06 12:53:19 +0300

    tp2

--- tp2/question1/FahrenheitCelsius.java
+++ tp2/question1/FahrenheitCelsius.java
@@ -15,15 +15,23 @@ public class FahrenheitCelsius {
 	 * @param args
 	 *            ...
 	 */
-	public static void main(String[] args) {
-		// pour tous les paramètres de la ligne de commande
-		int fahrenheit = 0;
-		float celsius = 0;
-		System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); // ligne,
-																				// format
-																				// imposés
-																				
-	}
+	public static void main(String[] args){
+       
+        int fahrenheit = 0;
+        float celsius = 0;
+        for(int i = 0; i<args.length; i++){
+            try{
+                    fahrenheit = java.lang.Integer.parseInt(args[i]);
+                    celsius = fahrenheitEnCelsius(fahrenheit);
+                    System.out.println(fahrenheit + "\u00B0F -> " +((int)(celsius*10)/10.0) + "\u00B0C");
+            }catch(NumberFormatException nfe){
+                   System.out.println("error : " + nfe.getMessage());
+              }
+       
+        }
+       
+     }
+
 
 	/**
 	 * la méthode à compléter.
@@ -33,9 +41,11 @@ public class FahrenheitCelsius {
 	 * @return la conversion en degré Celsius
 	 */
 	public static float fahrenheitEnCelsius(int f) {
+   
+        float e = (float)5/9*(f-32);
+        return e;
+    }   
 
-		return 0.F; // à compléter en remplaçant ce return 0.F par la fonction
-					// de conversion
 	}
 
 }
--- tp2/question2/FahrenheitCelsius.java
+++ tp2/question2/FahrenheitCelsius.java
@@ -14,25 +14,45 @@ public class FahrenheitCelsius{
       *
       *  @param args ...
       */
-     public static void main(String[] args){
-       try{
+      public static void main(String[] args){
        
-      
-       }catch(NumberFormatException nfe){
-           System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
-       }
+        int fahrenheit = 0;
+        float celsius = 0;
+        for(int i = 0; i<args.length; i++){
+            try{
+                    fahrenheit = java.lang.Integer.parseInt(args[i]);
+                    celsius = fahrenheitEnCelsius(fahrenheit);
+                    System.out.println(fahrenheit + "\u00B0F -> " +((int)(celsius*10)/10.0) + "\u00B0C");
+            }catch(NumberFormatException nfe){
+                   System.out.println("error : " + nfe.getMessage());
+              }
+       
+        }
        
      }
-     
-     /** 
-      * la méthode à compléter. 
-      *   @param f la valeur en degré Fahrenheit
-      *   @return  la conversion en degré Celsius
-      */
-     public static float fahrenheitEnCelsius( int f){
-       // ...
-       return 0.F;	// à compléter	en remplaçant la valeur retournée par la fonction de conversion
-       // ...
-     }
+
+     /**Test fahrenheitEnCelsius.
+     * 
+     * plusieurs tests de bon fonctionnement.
+     */
+    public void test_fahrenheitEnCelsius() {
+        assertEquals("    0 °F -> -17.7 °C ? ", -17.7, 
+			question1.FahrenheitCelsius.fahrenheitEnCelsius(0), 0.1);
+        assertEquals("  100 °F -> 37.7 °C ? ", 37.7, 
+			question1.FahrenheitCelsius.fahrenheitEnCelsius(100), 0.1);
+        assertEquals(" 2000 °F -> 1093.3 °C ?", 1093.3, 
+			question1.FahrenheitCelsius.fahrenheitEnCelsius(2000), 0.1);
+        assertEquals("   54 °F -> 12.2 °C ?", 12.2, 
+			question1.FahrenheitCelsius.fahrenheitEnCelsius(54), 0.1);
+        assertEquals("   37 °F -> 2.7 °C ?", 2.7, 
+			question1.FahrenheitCelsius.fahrenheitEnCelsius(37), 0.1);
+        assertEquals("   500 °F -> 260.0°C ?", 260.0, 
+			question1.FahrenheitCelsius.fahrenheitEnCelsius(500), 0.1);
+        assertEquals("   356 °F -> 180.0 °C ?", 180.0, 
+			question1.FahrenheitCelsius.fahrenheitEnCelsius(356), 0.1);
+        assertTrue("0°F < 100°F",question1.FahrenheitCelsius.fahrenheitEnCelsius(0)< 
+			question1.FahrenheitCelsius.fahrenheitEnCelsius(100));
+        
+    }
 
 }
